a308cbdaf6d1d47071eebbbca7cf004d756cf830
Felipe Braga <fbraga@pop-os.localdomain>
2022-06-07 19:58:59 -0300

    create application forms model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from .choices import *
+
+
+
+class FormAttention(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    questao1 = models.CharField(max_length=1, choices=QUESTAO_4_CHOICES, blank=False, null=False)
+    questao2 = models.CharField(max_length=1, choices=QUESTAO_4_CHOICES, blank=False, null=False)
+    questao3 = models.CharField(max_length=1, choices=QUESTAO_4_CHOICES, blank=False, null=False)
+    questao4 = models.CharField(max_length=1, choices=QUESTAO_4_CHOICES, blank=False, null=False)
+    questao5 = models.CharField(max_length=1, choices=QUESTAO_4_CHOICES, blank=False, null=False)
+
+
+class FormPersonality(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    questao1 = models.CharField(max_length=1, choices=QUESTAO_5_CHOICES, blank=False, null=False)
+    questao2 = models.CharField(max_length=1, choices=QUESTAO_5_CHOICES, blank=False, null=False)
+    questao3 = models.CharField(max_length=1, choices=QUESTAO_5_CHOICES, blank=False, null=False)
+
+
+class FormLearning(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    questao1 = models.CharField(max_length=1, choices=QUESTAO_6_CHOICES, blank=False, null=False)
+    questao2 = models.CharField(max_length=1, choices=QUESTAO_6_CHOICES, blank=False, null=False)
+    questao3 = models.CharField(max_length=1, choices=QUESTAO_6_CHOICES, blank=False, null=False)
+    questao6 = models.CharField(max_length=1, choices=QUESTAO_6_CHOICES, blank=False, null=False)
+    questao5 = models.CharField(max_length=1, choices=QUESTAO_6_CHOICES, blank=False, null=False)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 
+
 # Create your views here.
 def index(request):
     if request.method == 'GET':
